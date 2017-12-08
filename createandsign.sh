e7af368ba3e85db281859b1f9cb11caa97066e5d
root <root@huclis02.qvc-intern.de>
2017-12-08 10:36:03 +0100

    added jar signing script for lee taylor

---
+++ createandsign.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+
+if [[ -z $1 ]]; then
+  echo no Argument for jarfile given
+  path2jarfile=/opt/siebel/siebel811/sweapp/public/deu/21225/applets/SiebelToolbar.jar
+  echo "Using ${path2jarfile}"
+fi
+
+if [[ $1 == "-h" ]]; then
+  echo 'Usage: createandsign.sh [jarfile] [jarsigner]'
+  exit 
+fi
+
+if [[ -z $2 ]]; then
+  echo no Argument for jarsigner given, using:
+  if [[ -d /usr/oracle/product/11.2.0.3/client_1/jdk.old/bin ]]; then
+     echo "Directorycheck passed successfully"
+     path2jarsigner=/usr/oracle/product/11.2.0.3/client_1/jdk.old/bin/jarsigner
+     path2keytool=/usr/oracle/product/11.2.0.3/client_1/jdk.old/jre/bin/keytool
+     echo ${path2jarsigner}
+  else
+     path2jarsigner=`dzdo find / -name jarsigner | tail -n 1`
+     echo ${path2jarsigner}
+  fi
+else
+  path2jarsigner=$2
+  echo "Using Argument ${path2jarsigner}"
+fi
+
+echo "Using jarfile ${path2jarfile}"
+echo "Using signer ${path2jarsigner}"
+
+firstlastname='siebeluser'
+orgunit='Infrastructure'
+org='QVC'
+city='Liverpool'
+state='Merseyside'
+countrycode='UK'
+pass=lkwpeter
+
+dzdo rm -f /tmp/certdata
+dzdo cat <<EOF >/tmp/certdata
+${pass}
+${firstlastname}
+${orgunit}
+${org}
+${city}
+${state}
+${countrycode}
+yes
+
+EOF
+
+echo "Generated certdata in tmp"
+
+dzdo chown siebel:siebel /tmp/certdata
+
+echo "changed ownership of /tmp/certdata to user siebel"
+
+dzdo mv /root/.keystore /root/.keystore.`date +%s`
+
+
+dzdo su - root "-c ${path2keytool} -genkey -alias siebel -keyalg RSA -keystore /root/.keystore -keysize 4096 -validity 3650 </tmp/certdata"
+
+echo "=========created keystore as user siebel==========="
+
+
+dzdo ${path2keytool} -list -v -storepass ${pass} -keypass ${pass}
+
+echo "===============creating Copy of SiebelToolbar.jar=============="
+dzdo rm -rf /root/signing
+dzdo mkdir -p /root/signing
+dzdo cp ${path2jarfile} /root/signing/SiebelToolbar.jar
+echo "===============file was copied to /root/signing/==============="
+tempjarfile=/root/signing/SiebelToolbar.jar
+mycommand="${path2jarsigner} -storepass ${pass} -keypass ${pass} ${tempjarfile} siebel"
+
+# echo "executing... command ${mycommand}"
+
+tmpcmd="-c ${mycommand}"
+echo $tmpcmd
+dzdo su - root "${tmpcmd}"
+
+
+echo "----------executed command ${mycommand} with RC $?-----------"
